37b2a0aa334e60f329969f93990333a78e033d07
AnaisG14 <gatard.anais@gmail.com>
2022-06-13 22:33:16 +0200

    Creation of a new app lettings, migrate the 2 models 'address and letting' in oc_lettings_site and copy old data in the new table. Migrate the views lettings_index and letting and add urls in urls.py

--- oc_lettings_site/settings.py
+++ oc_lettings_site/settings.py
@@ -15,7 +15,6 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
-
 # Application definition
 
 INSTALLED_APPS = [
@@ -26,6 +25,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'lettings',
 ]
 
 MIDDLEWARE = [
